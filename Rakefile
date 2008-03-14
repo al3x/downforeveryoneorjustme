c1382572e213dba5704e172cf8fe0dae60360f1e
Alex Payne <al3x@al3x.net>
2008-03-13 17:08:43 -0700

    vlad basics, 404 and 50x pages

---
+++ Rakefile
@@ -0,0 +1,6 @@
+begin
+  require 'vlad'
+  Vlad.load :scm => :git
+rescue LoadError
+  # do nothing
+end
---
+++ config/deploy.rb
@@ -0,0 +1,4 @@
+set :application, "downforeveryorjustme"
+set :domain, "al3xpayne@downforeveryorjustme.com"
+set :deploy_to, "/var/www/downforeveryorjustme"
+set :repository, "git@github.com:al3x/downforeveryoneorjustme.git"
